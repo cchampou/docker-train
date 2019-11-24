ad2457987affa387acb77e97c53e2dfdb352a540
cchampou <clement@champouillon.com>
2019-11-24 20:44:52 +0100

    Converting into an hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(200).send("OK");
+	res.status(200).send("Hello world 👋");
 });
 
 app.listen(3000, () => {
